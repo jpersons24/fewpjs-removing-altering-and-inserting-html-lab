654ea3d6f33dc3a75a66237eef00bac54477be5f
Jakob Persons <jakobep24@gmail.com>
2021-01-11 21:19:58 -0500

    test passing

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+let body = document.querySelector('body')
+body.appendChild(newHeader)
+newHeader.innerHTML = 'Jake is the champion'
